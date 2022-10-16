2fcd08b919eb284e3040d25b93b3723aa3ab4e08
ravindrahv <ravindrahvasudeva@gmail.com>
2022-10-16 22:00:41 +0530

    initial commit

---
+++ IncrementProgram/src/Basics/Increment.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public class Increment {
+
+
+	public static void main(String[] args) {
+		int numA =5,numB=10,numC=0;
+		numC=--numC+numB--;
+        System.out.println(numA);
+        System.out.println(numB);
+        System.out.println(numC);
+
+	}
+
+}
+
+
+
