7fd774529a5108b1d4fb4bafb98ef68268581853
ravindrahv <ravindrahvasudeva@gmail.com>
2022-10-22 23:31:25 +0530

    initial commit

---
+++ ControlStatements/src/com/cg/demo/NestedStatement.java
@@ -0,0 +1,27 @@
+package com.cg.demo;
+
+public class NestedStatement {
+	public static void main(String[] args) {
+
+		String lang = "Java";
+
+		if(lang == "HLL & IL") {
+
+		if(lang =="python")
+
+		System.out.println("language is python");
+
+		}else if (lang == "C++") {
+
+		System.out.println("lang is C++");
+
+		}else if(lang == "Java") {
+
+		System.out.println("lang is Java");
+
+		}else {
+
+		System.out.println(lang); }
+
+		}
+}
