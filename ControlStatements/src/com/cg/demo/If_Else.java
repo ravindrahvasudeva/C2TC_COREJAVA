e0f7e3a3c26911ffd6348aeb069267a0f8ece107
ravindrahv <ravindrahvasudeva@gmail.com>
2022-10-24 09:21:21 +0530

    second commit

---
+++ ControlStatements/src/com/cg/demo/If_Else.java
@@ -0,0 +1,15 @@
+package com.cg.demo;
+
+public class If_Else {
+
+	public static void main(String[] args) {
+		int a=1;
+		if(a==1) {
+		System.out.println("hii");
+	}
+		else {
+			System.out.println("bye");
+		}
+	}
+
+}
