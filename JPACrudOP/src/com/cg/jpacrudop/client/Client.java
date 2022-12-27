95a073b3a1e9227afd8f4304f29d40cf6f085c66
ravindrahv <ravindrahvasudeva@gmail.com>
2022-12-27 16:12:07 +0530

    JPA-CRUD-OPERATION-DEMO

--- JPACrudOP/src/com/cg/jpacrudop/client/Client.java
+++ JPACrudOP/src/com/cg/jpacrudop/client/Client.java
@@ -11,29 +11,29 @@ public class Client {
 
 	        Student student = new Student();
 	        // Create Operation
-	        student.setStudentId(100);
-	        student.setName("Sachin");
+	        student.setStudentId(147);
+	        student.setName("Ravindra");
 	        service.addStudent(student);
 
 	        //at this breakpoint, we have added one record to table
 	        // Retrieve Operation
-	        student = service.findStudentById(100);
+	        student = service.findStudentById(147);
 	        System.out.print("ID:" + student.getStudentId());
 	        System.out.println(" Name:" + student.getName());
 	        // Update Operation
-	        student = service.findStudentById(100);
-	        student.setName("Sachin Tendulkar");
+	        student = service.findStudentById(147);
+	        student.setName("Ravindra");
 	        service.updateStudent(student);
 
 	        //at this breakpoint, we have updated record added in first section
 
-	        student = service.findStudentById(100);
+	        student = service.findStudentById(147);
 	        System.out.print("ID:" + student.getStudentId());
 	        System.out.println(" Name:" + student.getName());
 
 	        //at this breakpoint, record is removed from table
 	        // Delete Operation
-	        student = service.findStudentById(100);
+	        student = service.findStudentById(147);
 	        //service.removeStudent(student);
 	        System.out.println("End of program/Life cycle completed...");
 
